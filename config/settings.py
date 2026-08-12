03a7c0410b5117de05288532f7946ab4857a14f3
ragul-rajendran18 <ragul18rs@gmail.com>
2026-08-12 10:24:56 +0530

    changeATtimelimit

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+}
