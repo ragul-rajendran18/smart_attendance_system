fc311e6b9df3095ddf356c014d184b71725da132
Logesh R <142524635+logeshramesh05@users.noreply.github.com>
2026-08-25 16:12:34 +0530

    Update urls.py

--- attendance_system/urls.py
+++ attendance_system/urls.py
@@ -4,7 +4,6 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (
     login,
     me,
-    refresh_access_token,
     logout,
     admin_dashboard,
     create_student,
