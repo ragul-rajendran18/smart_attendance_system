e7d6e387c6a897e748f1177782534670796081a2
R Ragul <ragul18rs@gmail.com>
2026-08-25 21:29:26 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:5173",
     "http://0.0.0.0:8000",
+    "http://localhost:59322",
+    "http://127.0.0.1:59322",
 ]
 
 MIDDLEWARE = [
